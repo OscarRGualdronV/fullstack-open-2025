3ef17448377ffcc43f28342753eec822dd5c5ad3
OSCAR RAUL GUALDRON VILLAMIZAR <97969534+OscarRGualdronV@users.noreply.github.com>
2025-07-22 15:32:36 -0500

    Ejercicios del 2.6 - 2.10

---
+++ part2/src/components/PersonForm.jsx
@@ -0,0 +1,15 @@
+const PersonForm = ({ onSubmit, newName, handleNamechange, newNumber, handleNumberChange }) => (
+    <form onSubmit={onSubmit}>
+        <div>
+            name: <input value={newName} onChange={handleNamechange} />
+        </div>
+        <div>
+            number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+            <button type="submit">add</button>
+        </div>
+    </form>
+)
+
+export default PersonForm
